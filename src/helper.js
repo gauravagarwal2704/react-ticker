944b2de3b1127bcd4a0a8a9da101c8100c26af75
agarwal.gaurav2704@gmail.com <Gaurav Agarwal>
2017-12-06 15:27:45 +0530

    checking for tb

--- src/helper.js
+++ src/helper.js
@@ -27,8 +27,7 @@ let api_unocoin = axios.create({
     baseURL: 'https://www.unocoin.com/trade?all',
     timeout: 2000,
     headers:{'Access-Control-Allow-Origin':'*','Accept': 'application/json','Content-Type': 'application/json'}
-   
-// });
+});
 // Eventsource endpoint
 // const realtimeURL = `https://reactrealtime-6683.restdb.io/realtime?apikey=${apikey}`
 
