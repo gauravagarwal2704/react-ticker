94b302f2733c0f0bb5adb425544440333f36543a
gauravagarwal2704 <gauravrocks.1234@gmail.com>
2017-12-06 15:46:47 +0530

    Update helper.js

--- src/helper.js
+++ src/helper.js
@@ -14,13 +14,13 @@ let api_zebpay = axios.create({
 let api_throughbit_btc = axios.create({
     baseURL: 'https://www.throughbit.com/tbit_ci/index.php/cryptoprice/type/btc/inr',
     timeout: 2500,
-    headers:{'Accept': 'application/json', 'Content-Type': 'application/json'}
+    headers:{'Access-Control-Allow-Origin':'*','Accept': 'application/json', 'Content-Type': 'application/json'}
 });
 
 let api_throughbit_eth = axios.create({
     baseURL: 'https://www.throughbit.com/tbit_ci/index.php/cryptoprice/type/eth/inr',
     timeout: 2500,
-    headers:{'Accept': 'application/json','Content-Type': 'application/json'}
+    headers:{'Access-Control-Allow-Origin':'*', 'Accept': 'application/json','Content-Type': 'application/json'}
 });
 
 let api_unocoin = axios.create({
