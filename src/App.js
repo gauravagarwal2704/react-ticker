6efe77394089e80b0a2c6f661f0a4562222af9fc
agarwal.gaurav2704@gmail.com <Gaurav Agarwal>
2017-12-14 01:28:54 +0530

    Working except for unocoin with material-ui

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import {MuiThemeProvider, createMuiTheme } from 'material-ui/styles';
 // import _ from 'lodash';
 import './App.css';
+import fetchJsonp from 'fetch-jsonp';
+import Header from './Components/Header';
+import PriceCardContainer from './Components/PriceCardContainer';
 // import { api_zebpay, api_throughbit_btc, api_throughbit_eth, api_unocoin, update_delay } from './helper.js';
+const theme = createMuiTheme();
 
 class App extends Component {
 
-  constructor(){
-    super();
+  constructor(props){
+    super(props);
     this.state = {
       zebpay : {},
       tb_btc : {},
       tb_eth : {},
-      unocoin : {}
+      unocoin : 0,
+      data:[]
     }
-    //this.getTickerData();
+  }
+  
+  componentWillMount() {
     this.getTickerDataFetch();
   }
+
+
 
   getTickerDataFetch=()=> {
     return fetch('https://www.zebapi.com/api/v1/market/ticker/btc/inr')
@@ -27,20 +36,10 @@ class App extends Component {
           this.setState({
             zebpay : responseJson 
           });
+          this.updateData();
         }
         return responseJson;
       })
-      .then(fetch('https://www.throughbit.com/tbit_ci/index.php/cryptoprice/type/btc/inr')
-        .then(response => response.json())
-        .then(responseJson => {
-           console.log("tb_btc", responseJson);
-          if (responseJson){
-            this.setState({
-              tb_btc : responseJson.data.price[0] 
-            });
-          }
-          return responseJson;
-        }))
       .then(fetch('https://www.throughbit.com/tbit_ci/index.php/cryptoprice/type/eth/inr')
         .then(response => response.json())
         .then(responseJson => {
@@ -49,19 +48,47 @@ class App extends Component {
             this.setState({
               tb_eth : responseJson.data.price[0] 
             });
+            this.updateData();
           }
           return responseJson;
         }))
-      .then(fetch('https://www.unocoin.com/trade?buy',{
+      .then(fetch('https://www.throughbit.com/tbit_ci/index.php/cryptoprice/type/btc/inr')
+        .then(response => response.json())
+        .then(responseJson => {
+           console.log("tb_btc", responseJson);
+          if (responseJson){
+            this.setState({
+              tb_btc : responseJson.data.price[0] 
+            });
+            this.updateData();
+
+          }
+          return responseJson;
+        }))
+        .then(fetchJsonp('https://www.unocoin.com/trade?all')
+            .then(function(response) {
+              console.log('from-jsonp', response)
+              return response.json()
+            }).then(function(json) {
+              console.log('parsed json', json)
+            }).catch(function(ex) {
+              console.log('parsing failed', ex)
+        }))
+      
+      .then(fetch('https://www.unocoin.com/trade?all',{
         headers: {
             'Access-Control-Allow-Origin' : '*',
-            'Content-Type': 'application/json'
-        }
+            'Content-Type': 'application/json',
+        },
+        'dataType': "jsonp",
+        'timeout' : 5000,
+        'mode':'no-cors',
+        'async' : false
       })
-        .then(response => response.json())
         .then(responseJson => {
            console.log("unocoin", responseJson);
           if (responseJson){
+            console.log('Unocoin',responseJson)
             this.setState({
               unocoin : responseJson 
             });
@@ -73,43 +100,54 @@ class App extends Component {
       });
   }
 
+updateData = ()=>{
+        this.setState({
+          data:[
+            {
+              "name":"Zebpay",
+              "buy": this.state.zebpay.buy,
+              "sell":this.state.zebpay.sell,
+              "image" : "images/zebpay.png",
+              "coin" : "btc",
+              "coin_path" : 'images/btc.png'
+            },
+            {
+              "name":"ThroughBit BTC",
+              "buy":this.state.tb_btc.buy_price,
+              "sell":this.state.tb_btc.sell_price,
+              "image" : "images/throughbit.jpg",
+              "coin" : "btc",
+              "coin_path" : 'images/btc.png'
+            },
+            {
+              "name":"ThroughBit ETH",
+              "buy":this.state.tb_eth.buy_price,
+              "sell":this.state.tb_eth.sell_price,
+              "image" : "images/throughbit.jpg",
+              "coin" : "eth",
+              "coin_path" : 'images/eth.png'
+            }            
+          ]
+        });
+      }
+
+ 
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <button onClick={this.getTickerDataFetch}>Get Data</button>
-        <br/>
-        <div className="ticker-data">
-        <div>
-            <h3>Zebpay</h3>
-            <p>Buy Price : {this.state.zebpay.buy}</p>
-            <p>Sell Price : {this.state.zebpay.sell}</p>
-        </div>
-        <div>
-            <h3>Throughbit BTC</h3>
-            <p>Buy Price : {this.state.tb_btc.buy_price}</p>
-            <p>Sell Price : {this.state.tb_btc.sell_price}</p>
-        </div>
-        <div>
-            <h3>Throughbit ETH</h3>
-            <p>Buy Price : {this.state.tb_eth.buy_price}</p>
-            <p>Sell Price : {this.state.tb_eth.sell_price}</p>
-        </div>
-
-           
-
-           
-
-            
-        </div>
-            
-      </div>
+        <MuiThemeProvider theme={theme}>
+            <Header/>
+            <div className="App">
+            <br/>
+            <PriceCardContainer
+              clicked={this.getTickerDataFetch}
+              zebpay = {this.state.zebpay}
+              tb_btc = {this.state.tb_btc}
+              tb_eth = {this.state.tb_eth}
+              all_data = {this.state.data}
+            />
+          </div>
+        </MuiThemeProvider>
+      
     );
   }
 }
---
+++ src/Components/Header.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import AppBar from 'material-ui/AppBar';
+import Toolbar from 'material-ui/Toolbar';
+import Typography from 'material-ui/Typography';
+
+const Header = () => (
+      <AppBar position="static">
+        <Toolbar>
+          <Typography type="title" color="inherit" className=''>
+            CryptoTracker
+          </Typography>
+        </Toolbar>
+      </AppBar>
+);
+
+
+export default Header;
