630bddd9702b52c483cd0b4c16bef8e07992da95
gauravagarwal2704 <gauravrocks.1234@gmail.com>
2018-02-02 00:51:06 +0530

    Update fetch.js

--- src/Components/helpers/fetch.js
+++ src/Components/helpers/fetch.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = {
   fetchPrices:()=>{
-        return axios.get('https://react-ticker-backend.herokuapp.com/v1/prices')
+        return axios.get('https://cryptotracker-backend.herokuapp.com/v1/prices')
         .then(response => {
           console.log('response:',response.data);
           return response.data;
